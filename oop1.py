329aa85332ec7b6c15f0e2e3fff7ca3829425b33
victor yeswa <victoryeswa01@gmail.com>
2023-04-01 10:10:29 +0300

    Create oop1.py

---
+++ oop1.py
@@ -0,0 +1,20 @@
+#creation of an class
+#store management system ....in the picture
+class Item:
+    pass
+
+    #def calculate_total_price(self):
+
+#assigning attributes to instances
+item1 = Item()
+item1.name = 'Phone'
+item1.price = 100
+item1.quantity = 5
+
+
+item2 = Item()
+item2.name = 'laptop'
+item2.price = 1000
+item2.quantity = 3
+
+print(list(type(item2)))
