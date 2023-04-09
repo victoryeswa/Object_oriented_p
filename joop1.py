4e0655bf4f145ae11b7efa906a2ce8fad3c13dbf
victor yeswa <victoryeswa01@gmail.com>
2023-04-10 01:02:10 +0300

    Update joop1.py

--- joop1.py
+++ joop1.py
@@ -1,2 +1,25 @@
-import pandas as pd
-import numpy as np
+#object oriented programming in python 
+class Item:
+    pay_rate = 0.8 # The pay rate after 20% discount
+    def __init__(self, name: str, price: float, quantity=0):
+        # Run validations to the received arguments
+        assert price >= 0, f'Price {price} is not greater than or equal to zero!'
+        assert quantity >=0, f'Quantity {quantity} is not greater or equal to zero!'
+
+        #Assign to self object
+        self.name = name
+        self.price = price
+        self.quantity = quantity
+
+    def calculate_total_price(self):
+        return self.price * self.quantity
+    
+    def apply_discount(self):
+        self.price = self.price * self.pay_rate
+
+item1 = Item('Phone', 100, 1)
+item2 = Item('Laptop', 1000, 3)
+item3 = Item('Cable', 10, 5)
+item4 = Item('Mouse', 50, 5)
+item5 = Item('Keyboard', 75, 5)
+
