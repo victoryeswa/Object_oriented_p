e047bbc300a5788cba3ea96aac9d1cef4c01653c
victor yeswa <victoryeswa01@gmail.com>
2023-04-26 07:32:58 +0300

    Create danalysis.py

---
+++ danalysis.py
@@ -0,0 +1,26 @@
+#importing appropriate library
+import numpy as np
+import pandas as pd
+import seaborn as sns 
+import matplotlib.pyplot as plt
+#%matplotlib inline
+
+#reading the dataset
+fifa = pd.read_csv(r'WorldCups.csv')
+
+print(fifa)
+
+#showing the last ten record
+print(fifa.tail(10))
+
+#showing the first records of the dataset
+print(fifa.head(5))
+
+#finding the dimension of the dataset
+print(fifa.ndim, 'ndim')
+
+#showing the data types of the variable
+print(fifa.dtypes, 'dtypes')
+
+#using the info function
+print(fifa.info())
