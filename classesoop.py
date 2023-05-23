21cd5b1968a0b5a0c21d8d83e6c84fb4bbb4b1d7
victor yeswa <victoryeswa01@gmail.com>
2023-05-23 18:58:28 +0300

    Update classesoop.py

---
+++ classesoop.py
@@ -0,0 +1,11 @@
+import turtle
+
+class Polygon:
+    def __init__(self, name, size ):
+        self.sides = sides
+        self.name = name
+        self.size = size
+        self.interit_anges
+
+    def draw(self):
+        for i in range(self.sides)
