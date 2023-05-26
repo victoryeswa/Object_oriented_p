31f13e052bd13831c4259ae765d4a12df4bf4c04
victor yeswa <victoryeswa01@gmail.com>
2023-05-26 07:50:34 +0300

    Create oopmay4.py

---
+++ oopmay4.py
@@ -0,0 +1,15 @@
+class Cat:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+    def speak(self):
+        print('Meow')
+
+class Dog:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+    def speak(self):
+        print('Bark')
